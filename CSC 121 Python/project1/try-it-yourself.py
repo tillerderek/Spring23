47e962f4d9762a2c735b6a8259e675428206aa14
derek tiller <tillerderek@gmail.com>
2023-03-13 19:10:04 -0400

    half thru ch1

---
+++ CSC 121 Python/project1/try-it-yourself.py
@@ -0,0 +1,32 @@
+#15-1
+# import matplotlib.pyplot as plt
+
+# x_values = [1, 2, 3, 4, 5]
+# y_values = [x**3 for x in x_values]
+
+# fig, ax = plt.subplots()
+# ax.plot(x_values, y_values)
+
+# plt.show
+
+# import matplotlib.pyplot as plt
+
+# x_values = range(1, 6)
+# y_values = [x**3 for x in x_values]
+
+# fig, ax = plt.subplots()
+# ax.plot(x_values, y_values)
+
+# plt.show
+
+#15-2
+# import matplotlib.pyplot as plt
+
+# x_values = range(1, 6)
+# y_values = [x**3 for x in x_values]
+
+# fig, ax = plt.subplots()
+# ax.plot(x_values, y_values, c=(0.1, 0.8, 0.5))
+
+# plt.show
+
