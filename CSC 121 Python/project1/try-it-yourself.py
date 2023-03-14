f2ec62442df9fcde8d0b3809e92da870e3dcbdf4
derek tiller <tillerderek@gmail.com>
2023-03-14 14:18:14 -0400

    got to plotly

--- CSC 121 Python/project1/try-it-yourself.py
+++ CSC 121 Python/project1/try-it-yourself.py
@@ -30,3 +30,29 @@
 
 # plt.show
 
+#15-3
+
+import matplotlib.pyplot as plt
+
+from random_walk import RandomWalk
+while True:
+    # Make a random walk.
+    rw = RandomWalk()
+    rw.fill_walk()
+
+# Plot the points in the walk.
+    plt.style.use('classic')
+    fig, ax = plt.subplots(figsize=(15, 9))
+    point_numbers = range(rw.num_points)
+    ax.plot(rw.x_values, rw.y_values, color='blue', linewidth=2)
+    
+    #remove the axes.
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+
+    plt.show()
+
+    keep_running = input("Make another walk? (y/n): ")
+    if keep_running == 'n':
+        break
