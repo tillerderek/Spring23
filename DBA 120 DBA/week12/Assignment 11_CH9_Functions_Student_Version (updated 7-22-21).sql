59013f54fa71bc129665fea9a465769677a05cfd
tillerderek <tillerderek@gmail.com>
2023-04-10 11:13:07 -0400

    done

--- DBA 120 DBA/week12/Assignment 11_CH9_Functions_Student_Version (updated 7-22-21).sql
+++ DBA 120 DBA/week12/Assignment 11_CH9_Functions_Student_Version (updated 7-22-21).sql
@@ -65,16 +65,33 @@ SELECT ROUND(invoice_total, 1) AS "one decimal", ROUND(invoice_total, 0) AS "no 
 (28 pts.)
 --	3.	Write a SELECT statement that returns these columns from the Vendors table in the AP database:
 --			The vendor_name column 
---			The vendor_name column in all capital letters 
+--			The vendor_name column in all capital letters UCASE 
 --			The vendor_phone column 
 --			A column that displays the last four digits of each phone number
---
 --		When you get that working right, add the columns that follow to the result set.
 --		This is more difficult because these columns require the use of functions within functions.
 --			The vendor_phone column with the parts of the number separated by dots, as in 555.555.5555 
 --			### A column that displays the second word in each vendor name if there is one and blanks if there isn't  (YOU CAN SKIP THIS STEP!)
 --		Show SQL code and result here.
 
+SELECT vendor_name, UCASE(vendor_name), vendor_phone, SUBSTR(vendor_phone, -4), CONCAT(SUBSTR(vendor_phone, 2, 3), '.', SUBSTR(vendor_phone, 7, 3), '.', SUBSTR(vendor_phone, -4)) FROM vendors;
+
++---------------------------------+---------------------------------+----------------+--------------------------+----------------------------------------------------------------------------------------------------+
+-- | vendor_name                     | UCASE(vendor_name)              | vendor_phone   | SUBSTR(vendor_phone, -4) | CONCAT(SUBSTR(vendor_phone, 2, 3), '.', SUBSTR(vendor_phone, 7, 3), '.', SUBSTR(vendor_phone, -4)) |
+-- +---------------------------------+---------------------------------+----------------+--------------------------+----------------------------------------------------------------------------------------------------+
+-- | US Postal Service               | US POSTAL SERVICE               | (800) 555-1205 | 1205                     | 800.555.1205                                                                                       |
+-- | National Information Data Ctr   | NATIONAL INFORMATION DATA CTR   | (301) 555-8950 | 8950                     | 301.555.8950                                                                                       |
+-- | Register of Copyrights          | REGISTER OF COPYRIGHTS          | NULL           | NULL                     | NULL                                                                                               |
+-- | Jobtrak                         | JOBTRAK                         | (800) 555-8725 | 8725                     | 800.555.8725                                                                                       |
+-- | Newbrige Book Clubs             | NEWBRIGE BOOK CLUBS             | (800) 555-9980 | 9980                     | 800.555.9980                                                                                       |
+-- | California Chamber Of Commerce  | CALIFORNIA CHAMBER OF COMMERCE  | (916) 555-6670 | 6670                     | 916.555.6670                      
+-------------------------------------------------------------------------------------------------------------------------------------------
+-- | Dataforms/West                  | DATAFORMS/WEST                  | NULL           | NULL                     | NULL                                                                                               |
+-- | Zylka Design                    | ZYLKA DESIGN                    | (559) 555-8625 | 8625                     | 559.555.8625                                                                                       |
+-- | United Parcel Service           | UNITED PARCEL SERVICE           | (800) 555-0855 | 0855                     | 800.555.0855                                                                                       |
+-- | Federal Express Corporation     | FEDERAL EXPRESS CORPORATION     | (800) 555-4091 | 4091                     | 800.555.4091                                                                                       |
+-- +---------------------------------+---------------------------------+----------------+--------------------------+----------------------------------------------------------------------------------------------------+
+-- 122 rows in set (0.000 sec)
 
 
 (36 pts.)
@@ -90,5 +107,38 @@ SELECT ROUND(invoice_total, 1) AS "one decimal", ROUND(invoice_total, 0) AS "no 
 --		When you have this working, add a WHERE clause that retrieves just the invoices for the month of May
 --			 based on the invoice date, not the number of the invoice month.
 --		Show SQL code and result here.
-
-
+ SELECT invoice_number, invoice_date, DATE_ADD(invoice_date, INTERVAL 30 DAY), payment_date, DATEDIFF(payment_date, invoice_date) AS days_to_pay, MONTH(invoice_date), YEAR(invoice_date) FROM invoices WHERE invoice_date BETWEEN '2018-05-01' AND '2018-05-31';
+-- +----------------+--------------+-----------------------------------------+--------------+-------------+---------------------+--------------------+
+-- | invoice_number | invoice_date | DATE_ADD(invoice_date, INTERVAL 30 DAY) | payment_date | days_to_pay | MONTH(invoice_date) | YEAR(invoice_date) |
+-- +----------------+--------------+-----------------------------------------+--------------+-------------+---------------------+--------------------+
+-- | 25022117       | 2018-05-01   | 2018-05-31                              | 2018-06-10   |          40 |                   5 |               2018 |
+-- | P02-88D77S7    | 2018-05-03   | 2018-06-02                              | 2018-05-30   |          27 |                   5 |               2018 |
+-- | 21-4748363     | 2018-05-03   | 2018-06-02                              | 2018-05-22   |          19 |                   5 |               2018 |
+-- | 4-321-2596     | 2018-05-05   | 2018-06-04                              | 2018-06-05   |          31 |                   5 |               2018 |
+-- | 963253242      | 2018-05-06   | 2018-06-05                              | 2018-06-05   |          30 |                   5 |               2018 |
+-- | QP58872        | 2018-05-07   | 2018-06-06                              | 2018-05-19   |          12 |                   5 |               2018 |
+-- | 24863706       | 2018-05-10   | 2018-06-09                              | 2018-06-15   |          36 |                   5 |               2018 |
+-- | 10843          | 2018-05-11   | 2018-06-10                              | 2018-05-29   |          18 |                   5 |               2018 |
+-- | 963253235      | 2018-05-11   | 2018-06-10                              | 2018-06-09   |          29 |                   5 |               2018 |
+-- | 21-4923721     | 2018-05-13   | 2018-06-12                              | 2018-05-28   |          15 |                   5 |               2018 |
+-- | 77290          | 2018-05-13   | 2018-06-12                              | 2018-07-05   |          53 |                   5 |               2018 |
+-- | 963253246      | 2018-05-13   | 2018-06-12                              | 2018-06-09   |          27 |                   5 |               2018 |
+-- | 4-342-8069     | 2018-05-14   | 2018-06-13                              | 2018-06-13   |          30 |                   5 |               2018 |
+-- | 972110         | 2018-05-15   | 2018-06-14                              | 2018-05-27   |          12 |                   5 |               2018 |
+-- | 963253263      | 2018-05-16   | 2018-06-15                              | 2018-06-10   |          25 |                   5 |               2018 |
+-- | 121897         | 2018-05-19   | 2018-06-18                              | 2018-07-03   |          45 |                   5 |               2018 |
+-- | 1-200-5164     | 2018-05-20   | 2018-06-19                              | 2018-06-24   |          35 |                   5 |               2018 |
+-- | P02-3772       | 2018-05-21   | 2018-06-20                              | 2018-06-24   |          34 |                   5 |               2018 |
+-- | 97/486         | 2018-05-21   | 2018-06-20                              | 2018-06-22   |          32 |                   5 |               2018 |
+-- | 94007005       | 2018-05-23   | 2018-06-22                              | 2018-06-26   |          34 |                   5 |               2018 |
+-- | 963253232      | 2018-05-23   | 2018-06-22                              | 2018-06-18   |          26 |                   5 |               2018 |
+-- | RTR-72-3662-X  | 2018-05-25   | 2018-06-24                              | 2018-07-09   |          45 |                   5 |               2018 |
+-- | 97/465         | 2018-05-25   | 2018-06-24                              | 2018-06-24   |          30 |                   5 |               2018 |
+-- | 963253260      | 2018-05-25   | 2018-06-24                              | 2018-06-26   |          32 |                   5 |               2018 |
+-- | 963253272      | 2018-05-26   | 2018-06-25                              | 2018-06-30   |          35 |                   5 |               2018 |
+-- | 0-2058         | 2018-05-28   | 2018-06-27                              | 2018-06-30   |          33 |                   5 |               2018 |
+-- | 97/503         | 2018-05-30   | 2018-06-29                              | 2018-06-25   |          26 |                   5 |               2018 |
+-- | 963253255      | 2018-05-31   | 2018-06-30                              | 2018-06-27   |          27 |                   5 |               2018 |
+-- | 94007069       | 2018-05-31   | 2018-06-30                              | 2018-07-01   |          31 |                   5 |               2018 |
+-- +----------------+--------------+-----------------------------------------+--------------+-------------+---------------------+--------------------+
+-- 29 rows in set (0.001 sec)
