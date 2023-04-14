4b452bd8d74aa82c8eaa98a28926a7c1e8a115dc
derek tiller <tillerderek@gmail.com>
2023-04-14 15:04:13 -0400

    styling pt1

--- CSC 121 Python/project2/learning_log/learning_log/settings.py
+++ CSC 121 Python/project2/learning_log/learning_log/settings.py
@@ -34,6 +34,10 @@ INSTALLED_APPS = [
     # my apps
     'learning_logs',
     'users',
+
+    # third party apps
+    'bootstrap4',
+    
     # default django apps
     'django.contrib.admin',
     'django.contrib.auth',
